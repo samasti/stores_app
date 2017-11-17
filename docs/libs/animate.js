7b701eeab62ed24c668976c2d7997978b69ff6de
Braulio Bhavamitra <braulio@prout.io>
2017-11-17 05:53:28 -0300

    Add slide in/out animation to page manager

---
+++ docs/libs/animate.js
@@ -0,0 +1,12 @@
+'use strict';
+
+$.fn.extend({
+  animateCss: function animateCss(animationName, callback) {
+    var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+    this.addClass('animated ' + animationName).one(animationEnd, function () {
+      $(this).removeClass('animated ' + animationName);
+      if (callback) callback();
+    });
+    return this;
+  }
+});
