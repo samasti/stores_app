dd4416962deaad07b04201199b256f634b82fbcc
Braulio Bhavamitra <braulio@prout.io>
2017-11-18 07:17:14 -0300

    Add logo animation

--- docs/libs/animate.js
+++ docs/libs/animate.js
@@ -1,12 +1,22 @@
 'use strict';
 
 $.fn.extend({
-  animateCss: function animateCss(animationName, callback) {
+  animateCss: function animateCss(animationNames, callback) {
     var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-    this.addClass('animated ' + animationName).one(animationEnd, function () {
-      $(this).removeClass('animated ' + animationName);
-      if (callback) callback();
-    });
+    if (!Array.isArray(animationNames)) animationNames = [animationNames];
+
+    this.addClass('animated');
+    animationNames.forEach(function (e, index, array) {
+      this.queue(function (next) {
+        $(this).addClass(e).one(animationEnd, function () {
+          $(this).removeClass(e);
+          if (typeof callback === 'function' && index === array.length - 1) {
+            callback();
+          }
+          next();
+        });
+      });
+    }, this);
     return this;
   }
 });
--- libs/animate.es6
+++ libs/animate.es6
@@ -2,13 +2,22 @@
 ---
 
 $.fn.extend({
-  animateCss: function (animationName, callback) {
+  animateCss: function (animationNames, callback) {
     var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend'
-    this.addClass('animated ' + animationName).one(animationEnd, function() {
-      $(this).removeClass('animated ' + animationName)
-      if (callback)
-        callback()
-    })
+    if (!Array.isArray(animationNames)) animationNames = [animationNames]
+
+    this.addClass('animated')
+    animationNames.forEach(function (e, index, array) {
+      this.queue(function (next) {
+        $(this).addClass(e).one(animationEnd, function () {
+          $(this).removeClass(e)
+          if (typeof(callback) === 'function' && index === array.length - 1) {
+            callback()
+          }
+          next()
+        })
+      })
+    }, this)
     return this
   }
 })
